32ca5ccdee4fc2f53edd9c07f39faabf7cfac671
Thorsten Alteholz <github@alteholz.de>
2020-11-12 22:41:40 +0100

    create host entry as well

---
+++ manifests/create_nagios_host.pp
@@ -0,0 +1,44 @@
+#
+#    TDC: Test-driven configuration
+#
+#    define host entry for nagios
+#
+#    Copyright (C) 2020  Thorsten Alteholz
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation in version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+# @summary create host entry for nagios
+#
+# @example Basic usage
+#   define { 'tdc::create_nagios_host':
+#   }
+#
+# @param process
+#   Array of processes to be tested
+#
+
+define tdc::create_nagios_host (
+  String  $nagiosout   = "${::tdc::nagiosdir}/tdc-${::fqdn}",
+  String  $nagioshostuse = "generic-host",
+  String  $nagioshostdb = "${::tdc::nagiosdir}/hostdb.tdc",
+  String  $tdctitle = $title,
+) {
+#) inherits tdc {
+
+  Exec {
+    path    => ['/usr/bin', '/usr/sbin', '/bin'],
+  }
+
+ generate ("/bin/bash", "-c", "${::tdc::generator} ${nagiosout} host onlycreateonce $fqdn ${nagioshostuse} ${nagioshostdb}")
+}
